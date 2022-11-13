365cce2234c19b573dd0b5cdf147a1ba80c8db19
marcos-dalmasso <marcos.dalmasso@gmail.com>
2022-11-13 08:55:08 -0300

    where are you?

--- prueba.py
+++ prueba.py
@@ -1,5 +1,8 @@
 # this is my first file in github
 print ("who are you?...type your name please")
 name = input()
-print ("hello " + name +"!")
+print ('Hello ' + name +'!')
+print ('where are you from ' + name + '?')
+city = input()
+    
 
